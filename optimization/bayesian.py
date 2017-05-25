1d8299eab89eb1552559970aa29f2aa1de3b7af3
Tobias <tobias@Tobiass-MacBook.lan>
2017-05-24 21:33:41 -0700

    implemented query-based optimizer, but still has small bug

--- optimization/bayesian.py
+++ optimization/bayesian.py
@@ -1,37 +1,47 @@
 import GPyOpt
+import numpy as np
 
 from sklearn.base import BaseEstimator
 
-class BMPI(BaseEstimator):
-    def __init__(self, domain, n_iter=10, ac_func='MPI', verbosity=True):
-        self.n_iter = n_iter # number of iterations
+class BOE(BaseEstimator):
+    def __init__(self, domain,  ac_func='MPI'):
         self.domain = [{'name':'prompt', 'type':'bandit', 'domain':domain}] # domain to optimize over
         self.ac_func = ac_func # aquisition function
-        self.model_type = 'GP' # the function class to use as prior
-        self.initial_design_numdata = 0 # initial evaluations before optimization
-        self.evaluator_type = 'sequential' # optimization type
-        self.verbosity = verbosity # write status
+        self._X = None
+        self._Y = None
+        self.next_sample = None
 
     def get_params(self, deep=True):
-        return {"n_iter": self.n_iter, "domain": self.domain, "model":self.ac_func, "model_type":self.model_type, "initial_design_numdata":self.initial_design_numdata, "evaluator_type":self.evaluator_type, "verbosity":self.verbosity}
+        return { "domain": self.domain, "model":self.ac_func}
 
     def set_params(self, **parameters):
         for parameter, value in parameters.items():
             self.parameter = value
         return self
 
-    def optimize(self, func):
+    def optimize(self, x, y):
         # Functions handles the actual GP optimization via GPyOpt
-        # Set up the model
-        self.model = GPyOpt.methods.BayesianOptimization(f=func,
+        # Update the sampling history
+        self._update_history(x,y)
+
+        # Evaluate the model
+        self.model = GPyOpt.methods.BayesianOptimization(f = lambda x: np.ones((x.shape[0],1)), 
+                                                         X = self._X, 
+                                                         Y = self._Y,
                                                          normalize_Y=False,
-                                                         domain=self.domain, 
+                                                         domain=self.domain,
                                                          acquisition_type=self.ac_func)
 
-        # Run optimization (this should leads to repeated calls to the front-end)
-        self.model.run_optimization(self.n_iter)
+        # Update the next sample to evaluate model on
+        self.next_sample = self.model.suggested_sample[-1,:]
 
-        return self
+    def _update_history(self, x, y):
+        if self._X is not None and self._Y is not None:
+            self._X = np.vstack([self._X, x])
+            self._Y = np.vstack([self._Y, y])
+        else:
+            self._X = x
+            self._Y = y
 
 if __name__ == "__main__":
     pass
